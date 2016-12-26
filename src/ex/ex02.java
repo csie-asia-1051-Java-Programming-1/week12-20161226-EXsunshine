8a2ef59f0ae3e144b763b3ff8d3b5c230e89ded4
EXsunshine <105021057@live.asia.edu.tw>
2016-12-26 16:01:59 +0800

    030

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -4,12 +4,15 @@
  * Date: 2016/12/26
  * Author: 1050210XX 周永振老師
  */
-
+import java.util.*;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        int sum = 0;
+        char V = str.charAt(0);
 	}
 
 }
